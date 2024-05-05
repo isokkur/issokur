b4ecd83534771ecf507362bb365332361141fd5e
isokkur <159803960+isokkur@users.noreply.github.com>
2024-05-05 19:51:59 +0700

    2.0 Эмуляция и отслеживание пользовательского..

---
+++ Nastya/Nastya/Process.cs
@@ -0,0 +1,65 @@
+﻿
+using Xunit;
+
+public class MultiplicationMatrixTests
+{
+
+    [Fact]
+    public void TestMultiplicationMatrix()
+    {
+        // Arrange
+        int[,] expectedMatrix = {
+            {1, 2, 3},
+            {2, 4, 6},
+            {3, 6, 9},
+            {4, 8, 12}
+        };
+
+        // Act
+        int[,] actualMatrix = GenerateMultiplicationMatrix(3, 4);
+
+        // Assert
+        Assert.Equal(expectedMatrix, actualMatrix);
+    }
+
+    [Fact]
+    public void TestInvalidInput()
+    {
+        // Arrange
+        string expectedOutput = "Введены некорректные числа. Числа должны быть целыми положительными.";
+
+        // Act
+        string actualOutput = GetUserInputOutput("-1\n0\n3\n4");
+
+        // Assert
+        Assert.Equal(expectedOutput, actualOutput);
+    }
+
+    static int[,] GenerateMultiplicationMatrix(int x, int y)
+    {
+        int[,] matrix = new int[x, y];
+
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                matrix[i, j] = (i + 1) * (j + 1);
+            }
+        }
+
+        return matrix;
+    }
+
+    static string GetUserInputOutput(string input)
+    {
+        var consoleOutput = new System.IO.StringWriter();
+        Console.SetOut(consoleOutput);
+
+        var consoleInput = new System.IO.StringReader(input);
+        Console.SetIn(consoleInput);
+
+        MainClass.Main(new string[0]);
+
+        return consoleOutput.ToString().Trim();
+    }
+}
---
+++ Nastya/Nastya/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int x = GetUserInput("Введите целое число x:");
+        int y = GetUserInput("Введите целое число y:");
+
+        if (x < 1 || y < 1)
+        {
+            Console.WriteLine("Введены некорректные числа. Числа должны быть целыми положительными.");
+            return;
+        }
+
+        int[,] matrix = new int[x, y];
+
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                matrix[i, j] = (i + 1) * (j + 1);
+            }
+        }
+
+        Console.WriteLine("Матрица умножения:");
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static int GetUserInput(string message)
+    {
+        int input;
+        do
+        {
+            Console.WriteLine(message);
+        } while (!int.TryParse(Console.ReadLine(), out input));
+        return input;
+    }
+}
