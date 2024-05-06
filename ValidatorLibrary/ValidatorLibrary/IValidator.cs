66f9141df8667971054739abc56ecf7e45875201
isokkur <159803960+isokkur@users.noreply.github.com>
2024-05-06 12:07:48 +0700

    Валидация пароля и логина

---
+++ ValidatorLibrary/ValidatorLibrary/IValidator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidatorLibrary
+{
+    public interface IValidator
+    {
+        bool Validate(string? validateObject);
+    }
+
+    public class LoginValidator : IValidator
+    {
+        public bool Validate(string? validateObject)
+        {
+            if (string.IsNullOrWhiteSpace(validateObject))
+                return false;
+
+            return validateObject.Length >= 6 && validateObject.Length <= 16 && System.Text.RegularExpressions.Regex.IsMatch(validateObject, @"^[a-zA-Z]+$");
+        }
+    }
+
+    public class PasswordValidator : IValidator
+    {
+        public bool Validate(string? validateObject)
+        {
+            if (string.IsNullOrWhiteSpace(validateObject))
+                return false;
+
+            return validateObject.Length >= 8 && validateObject.Length <= 20 &&
+                   System.Text.RegularExpressions.Regex.IsMatch(validateObject, @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*-_])[A-Za-z\d!@#$%^&*-_]+$");
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class DataRowAttribute : Attribute
+    {
+        public object[] Data { get; }
+
+        public DataRowAttribute(params object[] data)
+        {
+            Data = data;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class DynamicDataAttribute : Attribute
+    {
+        public object[] Data { get; }
+
+        public DynamicDataAttribute(params object[] data)
+        {
+            Data = data;
+        }
+    }
+
+}
---
+++ ValidatorLibrary/ValidatorLibrary/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidatorLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoginValidator loginValidator = new LoginValidator();
+            PasswordValidator passwordValidator = new PasswordValidator();
+
+            // Тестирование с использованием атрибута DataRow
+            TestWithDataRow(loginValidator, passwordValidator);
+
+            // Тестирование с использованием атрибута DynamicData
+            TestWithDynamicData(loginValidator, passwordValidator);
+        }
+
+        [DataRow("User123", true)]
+        [DataRow("us123", false)]
+        static void TestWithDataRow(LoginValidator loginValidator, PasswordValidator passwordValidator)
+        {
+            object[] testParams = {
+                typeof(Program).GetMethod("TestWithDataRow").GetCustomAttributes(typeof(DataRowAttribute), false)[0],
+                new object[] { loginValidator, passwordValidator },
+            };
+
+            foreach (object testParam in testParams)
+            {
+                dynamic dataRow = testParam;
+                string validateObject = (string)dataRow.Data[0];
+                bool expectedResult = (bool)dataRow.Data[1];
+                bool result = loginValidator.Validate(validateObject);
+
+                Console.WriteLine($"Login Validation for '{validateObject}': Expected - {expectedResult}, Actual - {result}");
+            }
+        }
+
+        [DynamicData("User123", true)]
+        [DynamicData("us123", false)]
+        static void TestWithDynamicData(LoginValidator loginValidator, PasswordValidator passwordValidator)
+        {
+            object[] testParams = {
+                typeof(Program).GetMethod("TestWithDynamicData").GetCustomAttributes(typeof(DynamicDataAttribute), false)[0],
+                new object[] { loginValidator, passwordValidator },
+            };
+
+            foreach (object testParam in testParams)
+            {
+                dynamic dynamicData = testParam;
+                string validateObject = (string)dynamicData.Data[0];
+                bool expectedResult = (bool)dynamicData.Data[1];
+                bool result = loginValidator.Validate(validateObject);
+
+                Console.WriteLine($"Login Validation for '{validateObject}': Expected - {expectedResult}, Actual - {result}");
+            }
+        }
+
+    }
+}
